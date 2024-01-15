428e3e132136b48e4883ad16d737555f075438bb
Nahyun <tarea202001@gmail.com>
2024-01-15 23:26:23 +0900

    style : header css 완료

--- src/app/_component/Header/index.tsx
+++ src/app/_component/Header/index.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import styles from "./Header.module.css";
+
+import logo from "@/public/logo/logo.png";
+
 export default function Header() {
-  return <></>;
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.container}>
+          <div className={styles.left_container}>
+            <Image src={logo} alt="logo" width={100} height={100}></Image>
+            <div className={styles.nav_container}>
+              <div>여행</div>
+              <div>커뮤니티</div>
+            </div>
+          </div>
+          <div className={styles.right_container}>
+            <div>알림</div>
+            <div>로그인</div>
+            <div>마이페이지</div>
+            <div className={styles.logout}>로그아웃</div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
+import Header from "./_component/Header";
+import Footer from "./_component/Footer";
+
 export default function Home() {
-  return <>홈입니다.</>;
+  return (
+    <>
+      <div>홉입니다</div>
+    </>
+  );
 }
