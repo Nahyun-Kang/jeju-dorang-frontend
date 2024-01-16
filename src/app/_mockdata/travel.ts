a362d7215b4b83ebfea63201e21ac894abe48dbf
Nahyun-Kang <tarea202001@gmail.com>
2024-01-16 20:26:56 +0900

    feat :임시 저장

---
+++ src/app/_mockdata/travel.ts
@@ -0,0 +1,102 @@
+export const TravelMockData = [
+    {
+        name: '바당의물질',
+        tags: '특산물,착한가격 업소',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201804/30/98be55f3-fd3f-418b-b1ca-fcd3b0630170.jpg',
+    },
+    {
+        name: '캔북스',
+        tags:'커플, 혼자, 친구, 쇼핑, 사계, 청년, 휴식, 자유, 전시와 행사',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201908/28/afc61c17-901a-480c-943b-fb45cfc3fff8.JPG',
+    },
+    {
+        name: '리울',
+        tags:'구좌읍, 종달리, 소품샵, 그림, 에코백, 문구류',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202306/13/6d219f2f-f0c2-4a0f-af85-8d10475b72c7.jpg',
+    },
+    {
+        name: '제주그림',
+        tags:'쇼핑, 휴식/힐링, 공용주차장, 카드결제, 현금결제, 탐나는전, 영어, 쉬움',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202007/13/2d1af41d-49c0-40a7-b270-bdebb83fc07c.jpg',
+    },
+    {
+        name: '예술상점꺄르르',
+        tags:'서귀포, 인덕면, 사계리 소품샵, 의류, 잡화',
+        imageUrl: 'https://mblogthumb-phinf.pstatic.net/MjAyMTA3MjNfMjIx/MDAxNjI3MDI4MTAwMjYz.-9g-Ttv7irSSjyKtnIv00W3GAqSrgm5dWrP5zuvN8fog.e8lIg7onVtj0aEOB4ri6_YiHsuBOPziqb_EFruK-h_gg.JPEG.asdfy/20210716%EF%BC%BF180500.jpg?type=w800',
+    },
+    {
+        name: '책가방',
+        tags:'제주시내, 동문시장, 서점, 책방, 문구류',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202306/15/2be958d5-05c6-4a52-a468-5f03f47371b7.jpg',
+    },
+    {
+        name: '아름낭',
+        tags:'공방, 착한가격 업소',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201804/30/70f125b4-6aa2-4233-aa74-2a8d0669742e.jpg',
+    },
+    {
+        name: '승희상회',
+        tags:'불량식품, 과자, 쇼핑',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201812/20/0c0d2e39-fdff-4f79-95e7-8e01da7b8696.JPG',
+    },
+    {
+        name: '필름로그 제주직영점',
+        tags:'제주시내, 삼도동, 디지털카메라, 필름카메라, 카메라',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202303/21/0865fb2c-6139-4633-b844-f1e13c678944.jpg',
+    },
+    {
+        name: '바다보석',
+        tags:'쇼핑, 기념품',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201804/30/d0f247e9-a9ef-4714-9a7c-206c13c2b4d4.jpg',
+    },
+    {
+        name: '바당의물질',
+        tags: '특산물,착한가격 업소',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201804/30/98be55f3-fd3f-418b-b1ca-fcd3b0630170.jpg',
+    },
+    {
+        name: '캔북스',
+        tags:'커플, 혼자, 친구, 쇼핑, 사계, 청년, 휴식, 자유, 전시와 행사',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201908/28/afc61c17-901a-480c-943b-fb45cfc3fff8.JPG',
+    },
+    {
+        name: '리울',
+        tags:'구좌읍, 종달리, 소품샵, 그림, 에코백, 문구류',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202306/13/6d219f2f-f0c2-4a0f-af85-8d10475b72c7.jpg',
+    },
+    {
+        name: '제주그림',
+        tags:'쇼핑, 휴식/힐링, 공용주차장, 카드결제, 현금결제, 탐나는전, 영어, 쉬움',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202007/13/2d1af41d-49c0-40a7-b270-bdebb83fc07c.jpg',
+    },
+    {
+        name: '예술상점꺄르르',
+        tags:'서귀포, 인덕면, 사계리 소품샵, 의류, 잡화',
+        imageUrl: 'https://mblogthumb-phinf.pstatic.net/MjAyMTA3MjNfMjIx/MDAxNjI3MDI4MTAwMjYz.-9g-Ttv7irSSjyKtnIv00W3GAqSrgm5dWrP5zuvN8fog.e8lIg7onVtj0aEOB4ri6_YiHsuBOPziqb_EFruK-h_gg.JPEG.asdfy/20210716%EF%BC%BF180500.jpg?type=w800',
+    },
+    {
+        name: '책가방',
+        tags:'제주시내, 동문시장, 서점, 책방, 문구류',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202306/15/2be958d5-05c6-4a52-a468-5f03f47371b7.jpg',
+    },
+    {
+        name: '아름낭',
+        tags:'공방, 착한가격 업소',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201804/30/70f125b4-6aa2-4233-aa74-2a8d0669742e.jpg',
+    },
+    {
+        name: '승희상회',
+        tags:'불량식품, 과자, 쇼핑',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201812/20/0c0d2e39-fdff-4f79-95e7-8e01da7b8696.JPG',
+    },
+    {
+        name: '필름로그 제주직영점',
+        tags:'제주시내, 삼도동, 디지털카메라, 필름카메라, 카메라',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/202303/21/0865fb2c-6139-4633-b844-f1e13c678944.jpg',
+    },
+    {
+        name: '바다보석',
+        tags:'쇼핑, 기념품',
+        imageUrl: 'https://api.cdn.visitjeju.net/photomng/imgpath/201804/30/d0f247e9-a9ef-4714-9a7c-206c13c2b4d4.jpg',
+    },
+]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,21 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styles from "./home.module.css";
 
 import Header from "./_component/Header";
 import Footer from "./_component/Footer";
 
 export default function Home() {
   return (
-    <>
-      <div>홉입니다</div>
-    </>
+    <div className={styles.wrapper}>
+      <div>
+        <div className={styles.contents_wrapper}>
+          <div></div>
+          <div>
+            <div>title</div>
+            <p>tags</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
